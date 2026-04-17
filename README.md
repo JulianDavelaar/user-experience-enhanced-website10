8bd97e407b202d2dec42e9d5d2e6fd5267261c7d
juul <jdavelaar0@gmail.com>
2026-04-17 11:42:42 +0200

    #3 copy repo 9

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
+# Interactive Functionality
 
-# Enhanced website
-Ontwerp en maak een interactieve website die snel laadt en prettig te gebruiken is.
+Ontwerp en maak voor een opdrachtgever een interactieve toepassing die voor iedereen toegankelijk is
 
-De instructie vind je in: [INSTRUCTIONS.md](https://github.com/fdnd-task/enhanced-website/blob/main/docs/INSTRUCTIONS.md)
+De instructie vind je in: [INSTRUCTIONS.md](https://github.com/fdnd-task/the-web-is-for-everyone-interactive-functionality/blob/main/docs/INSTRUCTIONS.md)
 
 
 ## Inhoudsopgave
@@ -15,18 +15,42 @@ De instructie vind je in: [INSTRUCTIONS.md](https://github.com/fdnd-task/enhance
   * [Licentie](#licentie)
 
 ## Beschrijving
+Een uitleentool voor Cool kunst en cultuur om hierin bij te houden welke instrumenten en aan wie ze worden uitgeleend. 
+https://the-web-is-for-everyone-interactive-w3o0.onrender.com/
+<img width="1295" height="369" alt="image" src="https://github.com/user-attachments/assets/f4c90d23-f4d1-4a8b-a970-ba87a6395b52" />
+
 <!-- Bij Beschrijving staat kort beschreven wat voor project het is en wat je hebt gemaakt -->
-<!-- Voeg een mooie poster visual toe 📸 -->
-<!-- Voeg een link toe naar je live site 🌐-->
+<!-- Voeg een mooie poster visual of video toe 📸 -->
+<!-- Voeg een link toe naar GitHub Pages 🌐-->
 
 ## Gebruik
+Als gebruiker van de uitleensoftware van preludenfonds wil ik instrumenten op een overzichtelijke manier kunnen innemen en uitlenen om zo het overzicht te bewaren van alle instrumenten die er zijn.
+De uitleentool werkt als een lijst om snel te kunnen zien waar instrumenten zich bevinden, de status en eigenschappen van instrumenten kan aangepast worden en verbonden worden met een naam/contract om snel te kunnen zien bij wie het instrument zich bevind. 
 <!-- Bij Gebruik staat de user story, hoe het werkt en wat je er mee kan. -->
 
+## Ontwerpkeuzes & ideeën
+
+
+Bij het maken van deze website heb ik gewerkt volgens het principe van Progressive Enhancement. Dit betekent dat ik ben begonnen met een simpele basis die altijd werkt, en deze daarna stap voor stap heb uitgebreid.
+Ik ben gestart met alleen HTML, waarbij alle belangrijke functionaliteiten al werken zonder CSS of JavaScript. Denk aan het bekijken van instrumenten en het versturen van een aanvraag via een formulier. Door gebruik te maken van semantische elementen blijft de website duidelijk en bruikbaar in alle situaties.
+
+Daarna heb ik CSS toegevoegd om de website overzichtelijker en gebruiksvriendelijker te maken. De layout is verbeterd en er is visuele hiërarchie aangebracht, zodat het meteen duidelijk is wat belangrijk is. Ook zorgen states zoals hover en focus voor betere feedback bij interactie.
+Als laatste heb ik JavaScript gebruikt voor extra interactie en feedback. Deze laag is niet noodzakelijk voor de core functionaliteit, maar maakt de ervaring wel sneller en prettiger.
+
+Een bewuste keuze is dat formulieren via de server werken (POST requests), zodat alles altijd blijft functioneren zonder afhankelijk te zijn van JavaScript.
+Daarnaast heb ik rekening gehouden met verschillende UI states, zodat het voor de gebruiker altijd duidelijk is wat er gebeurt. Ook is de website mobile-first opgebouwd, waardoor ik vanuit een simpele basis ben gaan uitbreiden.
+
+Door deze aanpak blijft de website altijd werkend, toegankelijk en stap voor stap verbeterd.
+
 ## Kenmerken
-<!-- Bij Kenmerken staat welke technieken zijn gebruikt en hoe. Wat is de HTML structuur? Wat zijn de belangrijkste dingen in CSS? Wat is er met JS gedaan en hoe? Misschien heb je iets met NodeJS gedaan, of heb je een framwork of library gebruikt? -->
+Technieken: liquid, CSS, NodeJS, JS
+De Liquid structuur is zo simpel en overzichtelijk mogelijk gehouden hierin wordt gebruik gemaakt van tags om dynamische data te tonen, de css is uitgebreid en sluit goed aan op de liquid met de juiste class names. Met JS worden routes voor request en responses gemaakt om de database te kunnen updaten. 
+<!-- Bij Kenmerken staat welke technieken zijn gebruikt en hoe. Wat is de HTML structuur? Wat zijn de belangrijkste dingen in CSS? Wat is er met JS gedaan en hoe? Misschien heb je iets met NodeJS gedaan, of heb je een framework of library gebruikt? -->
 
 ## Installatie
+Mijn repo is te forken en kan door iedereen lokaal bewerkt worden. 
 <!-- Bij Installatie staat hoe een andere developer aan jouw repo kan werken -->
+
 
 ## Bronnen
 
--- server.js
+++ server.js
@@ -1,5 +1,153 @@
-console.log('Hier komt je server voor Sprint 10.')
+// Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
+// Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
+import express from 'express'
 
-console.log('Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
+// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
+import { Liquid } from 'liquidjs';
 
-console.log('Zet \'m op!')
+async function haalInstrumentenOp() {
+  const apiResponse = await fetch('https://fdnd-agency.directus.app/items/preludefonds_instruments')
+  const apiResponseJSON = await apiResponse.json()
+  return apiResponseJSON.data
+}
+
+
+// Maak een nieuwe Express applicatie aan, waarin we de server configureren
+const app = express()
+
+// Maak werken met data uit formulieren iets prettiger
+app.use(express.urlencoded({extended: true}))
+
+// Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
+// Bestanden in deze map kunnen dus door de browser gebruikt worden
+app.use(express.static('public'))
+
+// Stel Liquid in als 'view engine'
+const engine = new Liquid()
+app.engine('liquid', engine.express())
+app.set('view engine', 'liquid')
+
+// Stel de map met Liquid templates in
+// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
+app.set('views', './views')
+
+
+console.log('Let op: Er zijn nog geen routes. Voeg hier dus eerst jouw GET en POST routes toe.')
+
+// homepage
+app.get('/', async (request, response) => {
+   const instruments = await haalInstrumentenOp()
+
+   response.render('index', {
+   instruments: instruments
+  })
+})
+
+// detailpagina instrument
+app.get('/instrument/:id', async (request, response) => {
+  const id = Number(request.params.id)
+  const instruments = await haalInstrumentenOp()
+
+  const instrument = instruments.find(function(item) {
+    return item.id === id
+  })
+
+  if (!instrument) {
+    response.status(404).send('Instrument niet gevonden')
+    return
+  }
+
+  response.render('instrument-detail', {
+    instrument
+  })
+})
+
+
+// instrument toevoegen
+app.get('/instrument/toevoegen', (request, response) => {
+  response.render('instrument-toevoegen')
+})
+
+// instrument innemen
+app.get('/instrument/innemen', (request, response) => {
+  response.render('instrument-innemen')
+})
+
+// instrument uitlenen
+app.get('/instrument/uitlenen', async (request, response) => {
+  response.render('instrument-detail', {
+    instruments: instruments
+  })
+})
+
+/*
+// Zie https://expressjs.com/en/5x/api.html#app.get.method over app.get()
+app.get(…, async function (request, response) {
+  
+  // Zie https://expressjs.com/en/5x/api.html#res.render over response.render()
+  response.render(…)
+})
+*/
+
+/*
+// Zie https://expressjs.com/en/5x/api.html#app.post.method over app.post()
+app.post(…, async function (request, response) {
+
+  // In request.body zitten alle formuliervelden die een `name` attribuut hebben in je HTML
+  console.log(request.body)
+
+  // Via een fetch() naar Directus vullen we nieuwe gegevens in
+
+  // Zie https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch over fetch()
+  // Zie https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify over JSON.stringify()
+  // Zie https://docs.directus.io/reference/items.html#create-an-item over het toevoegen van gegevens in Directus
+  // Zie https://docs.directus.io/reference/items.html#update-an-item over het veranderen van gegevens in Directus
+  const fetchResponse = await fetch(…, {
+    method: …,
+    body: JSON.stringify(…),
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    }
+  })
+
+  // Als de POST niet gelukt is, kun je de response loggen. Sowieso een goede debugging strategie.
+  // console.log(fetchResponse)
+
+  // Eventueel kun je de JSON van die response nog debuggen
+  // const fetchResponseJSON = await fetchResponse.json()
+  // console.log(fetchResponseJSON)
+
+  // Redirect de gebruiker daarna naar een logische volgende stap
+  // Zie https://expressjs.com/en/5x/api.html#res.redirect over response.redirect()
+  response.redirect(303, …)
+})
+*/
+
+
+// Stel het poortnummer in waar Express op moet gaan luisteren
+// Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
+app.set('port', process.env.PORT || 8000)
+
+// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+  // Toon een bericht in de console
+  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)
+})
+
+
+app.post('/instrument/:id/uitlenen', async function(request, response){
+  const id = request.params.id
+  await fetch(`https://fdnd-agency.directus.app/items/preludefonds_instruments/${id}`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+    status: 'Uitgeleend'
+    }),
+
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    }
+  });
+
+  // Stuur de browser daarna weer naar de homepage
+  response.redirect(303, '/')
+})
